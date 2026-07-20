0784476d3e3fb8cdbf6735990366be05fbfd57e5
mehul-devops-engg <mehul200416@gmail.com>
2026-07-20 19:08:04 +0530

    Update admin-v2.js

--- js/admin-v2.js
+++ js/admin-v2.js
@@ -346,7 +346,7 @@ console.log("Trips received:", res.trips);
             tbody.innerHTML += `
                 <tr>
                     <td>${row.bookingId}</td>
-                    <td>${new Date(row.date).toLocaleDateString("en-GB")}</td>
+                    <td>${row.date}</td>
                     <td>${row.name}</td>
                     <td>${row.phone}</td>
                     <td>${row.car}</td>
@@ -544,3 +544,8 @@ document.getElementById("addBookingBtn").addEventListener("click", function () {
     document.getElementById("bookingModal").style.display = "flex";
 
 });
+function editBooking(bookingId){
+
+    alert("Editing Booking : " + bookingId);
+
+}
